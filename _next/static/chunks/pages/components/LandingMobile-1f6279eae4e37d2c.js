d0f7a067c382142257db1c6487614c568fc3ea59
Hardik <ninjacoderhardik@gmail.com>
2023-06-02 18:40:06 +0530

    Updates

---
+++ _next/static/chunks/pages/components/LandingMobile-1f6279eae4e37d2c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[906],{5163:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/LandingMobile",function(){return u(5232)}])}},function(n){n.O(0,[232,774,888,179],function(){return n(n.s=5163)}),_N_E=n.O()}]);
